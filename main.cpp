8c9cdd1178c523f0a39ed038c8f2b822698c6e5e
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-06-17 02:47:39 +0200

    Added decoration grid and two example tiles

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "Classes/Tile/Tile.hpp"
 
 /* Window configuration */
 struct WindowConfig {
@@ -13,6 +14,10 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(config.width, config.height), "Memory Game", sf::Style::Close);
     window.setFramerateLimit(60);  // 60 FPS
 
+    /* Example shapes */
+    Tile *tile1 = new Tile(sf::Vector2f(0, 0), 120, sf::Color::Magenta);
+    Tile *tile2 = new Tile(sf::Vector2f(120, 0), 120, sf::Color::Green);
+
     /* Window loop */
     while (window.isOpen()) {
         sf::Event event;
@@ -21,6 +26,37 @@ int main() {
                 window.close();
             }
         }
+
+        /* Clear window before drawing */
+        window.clear(sf::Color::White);
+
+        /* -------------------------------------------------- */
+        tile1->drawSprite(window);
+        tile2->drawSprite(window);
+
+        // Draw decoration grid
+        for (int i = 0; i < 5; i++) {
+            sf::Vertex line[] = {
+                sf::Vertex(sf::Vector2f(0.f, i * 120.f), sf::Color::Black),
+                sf::Vertex(sf::Vector2f(600.f, i * 120.f), sf::Color::Black)
+            };
+
+            window.draw(line, 2, sf::Lines);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            sf::Vertex line[] = {
+                sf::Vertex(sf::Vector2f(i * 120.f, 0.f), sf::Color::Black),
+                sf::Vertex(sf::Vector2f(i * 120.f, 600.f), sf::Color::Black)
+            };
+
+            window.draw(line, 2, sf::Lines);
+        }
+
+        /* -------------------------------------------------- */
+
+        /* Display all rendered elements */
+        window.display();
     }
 
     return 0;
