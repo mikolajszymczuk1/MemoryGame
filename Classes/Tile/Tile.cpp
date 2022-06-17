62de9446a715f90e3300413311e5b256df68f0cf
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-06-17 16:11:08 +0200

    Added overload for = operator, getTileColor, setNewColor methods

--- Classes/Tile/Tile.cpp
+++ Classes/Tile/Tile.cpp
@@ -1,17 +1,43 @@
 #include "Tile.hpp"
 
+/* Default constructor */
+Tile::Tile() { }
+
 /* Create rectangle shape with basic setup */
 Tile::Tile(sf::Vector2f position, int size, sf::Color color) {
     tileSize = size;
     tilePosition = position;
+    tileColor = color;
 
     rect = new sf::RectangleShape();
-    rect->setFillColor(color);
-    rect->setPosition(position);
-    rect->setSize(sf::Vector2f(size, size));
+    rect->setFillColor(tileColor);
+    rect->setPosition(tilePosition);
+    rect->setSize(sf::Vector2f(tileSize, tileSize));
 }
 
 /* Render sprite on screen */
-void Tile::drawSprite(sf::RenderWindow& window) {
+void Tile::drawSprite(sf::RenderWindow &window) {
     window.draw(*rect);
+}
+
+/* Overload '=' operator */
+Tile& Tile::operator=(const Tile *t) {
+    if (t != this) {
+        tileSize = t->tileSize;
+        tilePosition = t->tilePosition;
+        rect = t->rect;
+    }
+
+    return *this;
+}
+
+/* Return tile color */
+sf::Color Tile::getTileColor() {
+    return tileColor;
+}
+
+/* Set new color value for tile */
+void Tile::setNewColor(sf::Color newColor) {
+    tileColor = newColor;
+    rect->setFillColor(newColor);
 }
--- Classes/Tile/Tile.hpp
+++ Classes/Tile/Tile.hpp
@@ -4,9 +4,14 @@
 class Tile {
     private:
         int tileSize;
+        sf::Color tileColor;
         sf::Vector2f tilePosition;
         sf::RectangleShape *rect;
     public:
+        Tile();
         Tile(sf::Vector2f position, int size, sf::Color color);
-        void drawSprite(sf::RenderWindow& window);
+        void drawSprite(sf::RenderWindow &window);
+        Tile& operator=(const Tile *t);
+        sf::Color getTileColor();
+        void setNewColor(sf::Color newColor);
 };
