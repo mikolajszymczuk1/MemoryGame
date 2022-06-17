e1433888bc4db4a2265fcad38324160e1f1c51e9
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-06-17 02:47:58 +0200

    Updated compilation script

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 # Simple script to compile all app
 g++ -c main.cpp
-g++ main.o -o MemoryGameApp -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c Classes/Tile/Tile.cpp
+g++ Tile.o main.o -o MemoryGameApp -lsfml-graphics -lsfml-window -lsfml-system
